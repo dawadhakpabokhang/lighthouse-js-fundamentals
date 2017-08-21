d53422c6f39fc8439f60172dec7685a7c4575f64
dawadhakpabokhang <dawabokhang@gmail.com>
2017-08-21 14:13:59 +0000

    Adds functions exercises

---
+++ even.js
@@ -0,0 +1,20 @@
+// Version 1 //
+
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
+
+// var tenIsEven = isEven(10);
+// var elevenIsEven = isEven(11);
+
+// console.log(tenIsEven);
+// console.log(elevenIsEven);
+
+// Version 3 //
+
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
