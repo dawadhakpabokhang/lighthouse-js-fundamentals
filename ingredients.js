f93ac06d35081f1742bb81c4214cc0e28e262f7a
dawadhakpabokhang <dawabokhang@gmail.com>
2017-08-21 21:28:34 +0000

    Ingredients loops

---
+++ ingredients.js
@@ -0,0 +1,21 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+var counter = 0;
+
+// while loop
+
+// while (counter < ingredients.length){
+//   console.log(ingredients[counter]);
+//   counter++;
+// }
+
+// for loop
+
+// for (counter = 0; counter < ingredients.length; counter++){
+//   console.log(ingredients[counter]);
+// }
+
+// for loop reversed
+
+for (counter = ingredients.length-1 ; counter >= 0; counter--){
+  console.log(ingredients[counter]);
+}
